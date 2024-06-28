6ac5ffe420c2421c67f8acd395e88fd4f08485d5
prodcutivesa <salahzeinu@gmail.com>
2024-06-28 12:33:05 -0700

    3status

---
+++ api/v1/views/index.py
@@ -0,0 +1,33 @@
+
+from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+@app_views.route('/status')
+def get_status():
+    '''
+    '''
+    return jsonify(status='OK')
+
+
+@app_views.route('/stats')
+def get_stats():
+    '''
+    '''
+    objects = {
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review,
+        'states': State,
+        'users': User
+    }
+    for key, value in objects.items():
+        objects[key] = storage.count(value)
+    return jsonify(objects)
